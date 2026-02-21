c1a3f165bf02b1d721672183959fde05839b20cd
andrew <andrewalangm@gmail.com>
2026-02-20 20:06:47 -0600

    Store data in the JS hook and play battle sound

--- assets/js/pokemon.hooks.js
+++ assets/js/pokemon.hooks.js
@@ -1,8 +1,18 @@
+const BATTLE_SOUND = "/sounds/pokemon_battle.mp3";
 const PokemonBattle = {
+  battleData: null,
+  battleSound: null,
   mounted() {
     this.handleEvent("battle:start", (payload) => {
-      console.log(payload);
+      this.battleData = payload;
+      this.battleSound = this.playSound(BATTLE_SOUND, 5);
     });
+  },
+  playSound(src, duration) {
+    const audio = new Audio(src);
+    audio.duration = duration;
+    audio.play();
+    return audio;
   },
 };
 
